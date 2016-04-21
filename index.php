4cd16d3568bea5f88dcbc7fb791c2b50fb1ec7d7
gbutiri <moviemaker713@gmail.com>
2016-04-21 16:37:49 -0700

    Added files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!document html>
+<html>
+
+<head>
+	<title>Iso Town</title>
+</head>
+
+<body>
+	
+	<div id="game"></div>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+	<script src="/crafty.min.js"></script>
+	<script src="/main.js"></script>
+	<script>
+		
+	</script>
+
+</body>
+
+</html>
